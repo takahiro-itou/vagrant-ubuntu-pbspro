2673cf643395b3b97368258c0e97d61cd3f47b65
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-20 12:53:52 +0900

    ワーカーノードの設定を追加

--- data/common/config/worker-config-02.sh
+++ data/common/config/worker-config-02.sh
@@ -1,3 +1,15 @@
 #!/bin/bash  -xue
 
 scriptDir=$(dirname  $0)
+
+sudo  service pbs start    ||  exit  $?
+
+pushd  ${scriptDir}        ||  exit  $?
+
+sudo  /bin/bash  -xu  ./mom_reset_config   ||  exit  $?
+sudo  /bin/bash  -xu  ./mom_create_vnodes  ||  exit  $?
+
+sudo  service pbs restart  ||  exit  $?
+
+popd
+sleep  5
