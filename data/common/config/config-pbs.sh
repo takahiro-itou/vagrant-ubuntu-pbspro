17e2eccc8048f28317e695697c9714dd86d9173e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-20 11:40:02 +0900

    設定ファイル /etc/pbs.conf  を編集

--- data/common/config/config-pbs.sh
+++ data/common/config/config-pbs.sh
@@ -1,3 +1,13 @@
 #!/bin/bash  -xue
 
 scriptDir=$(dirname  $0)
+
+#################################################################
+##
+##  Setup Master.
+##
+
+sudo  sed  -i.bak  \
+    -f ${scriptDir}/setup-pbsconf-master.sed  \
+    /etc/pbs.conf  ||  exit  $?
+echo  'PBS_SERVER_HOST_NAME=node00'  |  sudo  tee -a  /etc/pbs.conf
