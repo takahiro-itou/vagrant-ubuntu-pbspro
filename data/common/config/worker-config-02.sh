649d995a873aeeaddb1839aa85169af871fc7892
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-20 13:15:00 +0900

    不具合修正

--- data/common/config/worker-config-02.sh
+++ data/common/config/worker-config-02.sh
@@ -2,14 +2,12 @@
 
 scriptDir=$(dirname  $0)
 
-sudo  service pbs start    ||  exit  $?
 
-pushd  ${scriptDir}        ||  exit  $?
-
-sudo  /bin/bash  -xu  ./mom_reset_config   ||  exit  $?
-sudo  /bin/bash  -xu  ./mom_create_vnodes  ||  exit  $?
-
-sudo  service pbs restart  ||  exit  $?
+pushd  ${scriptDir}                         ||  exit  $?
+sudo  /bin/bash  -xu  ./mom-reset-config    ||  exit  $?
+sudo  service  pbs  start                   ||  exit  $?
+sudo  /bin/bash  -xu  ./mom-create-vnodes   ||  exit  $?
+sudo  service  pbs  restart                 ||  exit  $?
 
 popd
 sleep  5
