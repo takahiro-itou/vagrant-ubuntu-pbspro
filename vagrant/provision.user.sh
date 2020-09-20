51574fdfa59c622f821d93b97559a96dd68726a6
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-20 11:49:50 +0900

    不具合修正
    (1) 転送時のディレクトリ構造の間違いを修正

--- vagrant/provision.user.sh
+++ vagrant/provision.user.sh
@@ -3,11 +3,16 @@
 echo  Provisioning  ${USER} for ${HOSTNAME}
 test  -f  ${HOME}/.provision.user  &&  exit 0
 
-for srcDir in  /tmp/data/common/config  ; do
+for  srcDir  in  /tmp/data/common/config  ; do
     if  test -d ${srcDir} ; then
         pushd  ${srcDir}
         chmod  -R   755  *.sh
         popd
+    fi
+done
+
+for  srcDir  in  /tmp/data/common  ; do
+    if  test -d ${srcDir} ; then
         rsync  -av  ${srcDir}/  ${HOME}/
     fi
 done
