51633335cf7fe684765d99aca3df6820402108ac
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-21 19:41:01 +0900

    ホームディレクトリのパーミッションを修正

--- vagrant/provision.user.sh
+++ vagrant/provision.user.sh
@@ -13,8 +13,15 @@ done
 
 for  srcDir  in  /tmp/data/common  ; do
     if  test -d ${srcDir} ; then
+        chmod  -R   go-rwx  ${srcDir}
         rsync  -av  ${srcDir}/  ${HOME}/
     fi
 done
+
+if  test -d ${HOME}/.ssh ; then
+    chmod  -R   go-rwx  ${HOME}/.ssh/
+    chmod  700  ${HOME}/.ssh
+fi
+chmod  700  ${HOME}
 
 date  >  ${HOME}/.provision.user
